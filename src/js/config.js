c2e6bb6985906323a9bc117b4960f169779128a5
Dagi Ameha <dagimameha@gmail.com>
2024-08-18 23:12:18 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3d2b8356-4fa6-4aaf-a0f2-cd41167cc4c5';
+export const MODAL_CLOSE_SEC = 2.5;
