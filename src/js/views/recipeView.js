1c4a8a5667dffcfe8753a149ec42a8559a15e2e5
DagiAmeha <162983714+DagiAmeha@users.noreply.github.com>
2025-10-27 23:33:47 +0300

    Update recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,8 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+import Fraction from 'fractional';
+
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -121,14 +122,14 @@ class RecipeView extends View {
     `;
   }
 
-  _generateMarkupIngredient(ing) {
-    return `
+ _generateMarkupIngredient(ing) {
+  return `
     <li class="recipe__ingredient">
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? new Fraction.Fraction(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
@@ -136,7 +137,8 @@ class RecipeView extends View {
       </div>
     </li>
   `;
-  }
+}
+
 }
 
 export default new RecipeView();
