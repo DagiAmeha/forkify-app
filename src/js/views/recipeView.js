45986f260f4dbf463af92bf11b420c2b1b89af5d
DagiAmeha <162983714+DagiAmeha@users.noreply.github.com>
2025-11-02 13:37:16 +0300

    Update recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -102,9 +102,9 @@ class RecipeView extends View {
     return `
       <li class="recipe__ingredient">
         <svg class="recipe__icon"><use href="${icons}#icon-check"></use></svg>
-        <div class="recipe__quantity">
-          ${ing.quantity ? new Fraction(ing.quantity).toString() : ''}
-        </div>
+        // <div class="recipe__quantity">
+        //   ${ing.quantity ? new Fraction(ing.quantity).toString() : ''}
+        // </div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}
